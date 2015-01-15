f2c24dc2fa7b3a913d524ab5f9593ac51090c6e7
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-15 14:18:41 -0600

    Added animation and sun array and current image array

--- GoodnightMoon/ViewController.m
+++ GoodnightMoon/ViewController.m
@@ -12,7 +12,18 @@
 @interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
 @property NSMutableArray *moonImagesArray;
+@property NSMutableArray *sunImagesArray;
+@property NSMutableArray *currentImagesArray;
+
+
+
 @property (strong, nonatomic) IBOutlet UIView *shadeView;
+
+@property (strong, nonatomic) UICollisionBehavior *collisionBehavior;
+@property (strong, nonatomic) UIDynamicItemBehavior *dynamicItemBehavior;
+@property (strong, nonatomic) UIGravityBehavior *gravityBehavior;
+@property (strong, nonatomic) UIDynamicAnimator *dynamicAnimator;
+@property (strong, nonatomic) UIPushBehavior *pushBehavior;
 
 @end
 
@@ -28,6 +39,42 @@
     [self.moonImagesArray addObject:[UIImage imageNamed:@"moon_4"]];
     [self.moonImagesArray addObject:[UIImage imageNamed:@"moon_5"]];
     [self.moonImagesArray addObject:[UIImage imageNamed:@"moon_6"]];
+
+    self.sunImagesArray = [NSMutableArray new];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_1"]];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_2"]];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_3"]];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_4"]];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_5"]];
+    [self.sunImagesArray addObject:[UIImage imageNamed:@"sun_6"]];
+
+    self.currentImagesArray = self.moonImagesArray;
+
+
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    self.dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+
+    self.collisionBehavior = [[UICollisionBehavior alloc] initWithItems:[NSArray arrayWithObject:self.shadeView]];
+    self.gravityBehavior = [[UIGravityBehavior alloc] initWithItems:[NSArray arrayWithObject:self.shadeView]];
+    self.dynamicItemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:[NSArray arrayWithObject:self.shadeView]];
+    self.pushBehavior = [[UIPushBehavior alloc] initWithItems:[NSArray arrayWithObject:self.shadeView] mode:UIPushBehaviorModeContinuous];
+
+    [self.collisionBehavior addBoundaryWithIdentifier:@"bottom"
+                                            fromPoint:CGPointMake(0, self.view.frame.size.height)
+                                              toPoint:CGPointMake(self.view.frame.size.width, self.view.frame.size.height)];
+
+
+
+    [self.gravityBehavior setGravityDirection:CGVectorMake(0, 0)];  // no gravity when the view loads
+
+    [self.dynamicItemBehavior setElasticity:0.25];   // for bouncing off the boundaries
+
+    [self.dynamicAnimator addBehavior:self.collisionBehavior];
+    [self.dynamicAnimator addBehavior:self.gravityBehavior];
+    [self.dynamicAnimator addBehavior:self.pushBehavior];
+    [self.dynamicAnimator addBehavior:self.dynamicItemBehavior];
 }
 
 - (IBAction) handlePan:(UIPanGestureRecognizer *)gesture {
@@ -35,18 +82,44 @@
     gesture.view.center = CGPointMake(gesture.view.center.x, gesture.view.center.y + point.y);
     [gesture setTranslation:CGPointMake(0, 0) inView:gesture.view];
 
+    CGFloat yVelocity = [gesture velocityInView:gesture.view].y;  // get the y velocity
+
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+
+        [self.dynamicAnimator updateItemUsingCurrentState:self.shadeView];
+
+        if (yVelocity < -500.0) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, -1)];
+            [self.dynamicItemBehavior setElasticity:0.5];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, [gesture velocityInView:gesture.view].y)];
+        }
+        else if (yVelocity >= -500.0 && yVelocity < 0) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, -1)];
+            [self.dynamicItemBehavior setElasticity:0.25];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, -500.0)];
+        }
+        else if (yVelocity >= 0 && yVelocity < 500.0) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, 1)];
+            [self.dynamicItemBehavior setElasticity:0.25];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, 500.0)];
+        } else {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, 1)];
+            [self.dynamicItemBehavior setElasticity:0.5];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, [gesture velocityInView:gesture.view].y)];
+        }
+    }
 }
 
 
 #pragma mark UICollectionViewDataSource
 
 - (NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.moonImagesArray.count;
+    return self.currentImagesArray.count;
 }
 
 - (UICollectionViewCell *) collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     ImageCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CellID" forIndexPath:indexPath];
-    cell.imageView.image = [self.moonImagesArray objectAtIndex:indexPath.row];
+    cell.imageView.image = [self.currentImagesArray objectAtIndex:indexPath.row];
     
     return cell;
 }
